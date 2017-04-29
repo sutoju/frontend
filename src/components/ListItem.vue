f6c03799ac32858274474ce613c59478fe175174
Jere Vaara <jerevaara@gmail.com>
2017-04-29 22:39:42 +0900

    ugly recipe items

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="item">
-    <div class="title">{{ title }}</div>
     <div class="image" :style="{ 'background-image': 'url('+imagesrc+')' }">
+      <div class="title">
+        <div class="word" v-for="word in words">{{ word }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,35 +11,58 @@
 <script>
 export default {
   props: ['title', 'imagesrc'],
-  name: 'list-item'
+  name: 'list-item',
+  computed: {
+    words: function() {
+      if (this.title) {
+        return this.title.split(' ')
+      }
+      return []
+    }
+  }
 }
 
 </script>
 
 <style scoped lang="scss">
 @import '../main.scss';
+
 .item {
+  @extend .source-sans;
   position: relative;
   border-radius: 10px;
   border: 1px solid #dedede;
   margin: $small 0;
-  padding: $medium;
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 100px;
+  overflow: hidden;
 
+  @media (max-width: 767px) {
+    height: 160px;
+
+    .title {
+      line-height: 0.9;
+      font-size: 40pt;
+      color: transparentize(#333, 0.1);
+
+      .word {
+        display: inline-block;
+        background: white;
+      }
+    }
+  }
 }
 
 .image {
   flex: 1;
   background-position: center;
   background-repeat: no-repeat;
-  background-size: contain;
+  background-size: 100%;
 }
 
 .title {
-  font-size: 16pt;
   text-transform: capitalize;
 }
 
